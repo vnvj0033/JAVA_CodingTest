deccf9c6d4133377963ed588db0512117428f33c
vnvj0033 <vnvj0033@gmail.com>
2021-04-14 23:15:10 +0900

    Add. MoveZeros

---
+++ src/MoveZeros.java
@@ -0,0 +1,24 @@
+public class MoveZeros {
+
+    public static void main(String args[]) {
+        int[] nums = {0, 3, 2, 0, 8, 5};
+
+        solve(nums);
+        for (int num : nums)
+            System.out.print(num + ",");
+
+    }
+
+    private static int[] solve(int[] nums) {
+        int currentIndex = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] != 0)
+                nums[currentIndex++] = nums[i];
+
+        while (currentIndex < nums.length)
+            nums[currentIndex++] = 0;
+
+        return nums;
+    }
+}
