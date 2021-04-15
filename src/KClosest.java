2a3cc58b189d9f9d3a3b5b13391de69bc9f9537c
vnvj0033 <vnvj0033@gmail.com>
2021-04-15 20:48:10 +0900

    Add. KClosest

---
+++ src/KClosest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class KClosest {
+
+    public static void main(String[] args) {
+        int[][] points = {{1, 3}, {-2, 2}};
+        int k = 1;
+        int[][] result = new KClosest().solve(points, k);
+
+        System.out.println(result[0][0] + ", " + result[0][1]);
+    }
+
+    public int[][] solve(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1]);
+        return Arrays.copyOfRange(points, 0, k);
+    }
+}
