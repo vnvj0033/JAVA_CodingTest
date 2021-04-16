aee960a1ebc12b6263ba8a734bf5a3ccd5dd6a4b
vnvj0033 <vnvj0033@gmail.com>
2021-04-16 23:04:41 +0900

    Add.AddTwoNumbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,63 @@
+
+/**
+ * 노드
+ * 2 - 4 - 3
+ * 5 - 6 - 2
+ * 결과
+ * 7 - 0 - 6
+ * */
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(2);
+
+        ListNode node = solve(l1, l2);
+
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public static ListNode solve(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode(0);
+        ListNode p3 = newHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+
+            p3.next = new ListNode(carry % 10);
+            p3 = p3.next;
+            carry /= 10;
+        }
+
+        if (carry == 1) p3.next = new ListNode(1);
+
+        return newHead.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+        }
+    }
+}
