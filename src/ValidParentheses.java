9cf1ab0018cc562a18a3b196e7858c20a1e6e3de
vnvj0033 <vnvj0033@gmail.com>
2021-04-17 17:30:37 +0900

    Add. ValidParentheses

---
+++ src/ValidParentheses.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+
+        String exp = "(({([])})){}{{}}(()){}";
+        System.out.println(solve(exp));
+    }
+
+
+    public static boolean solve(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: str.toCharArray()) {
+            if(c=='(')
+                stack.push(')');
+            else if(c=='[')
+                stack.push(']');
+            else if(c=='{')
+                stack.push('}');
+            else if(stack.isEmpty()|| stack.pop() != c)
+                return false;
+
+
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid_map(String str) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('{', '}');
+        map.put('(', ')');
+        map.put('[', ']');
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            if(map.containsKey(c)) {
+                stack.push(c);
+            }else if(map.containsValue(c)) {
+                if(!stack.isEmpty() && map.get(stack.peek())==c) {
+                    stack.pop();
+                }else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
