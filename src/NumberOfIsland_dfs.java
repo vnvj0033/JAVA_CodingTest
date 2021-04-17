368462968d2ad5791974ee316789e08622f17473
vnvj0033 <vnvj0033@gmail.com>
2021-04-17 18:03:13 +0900

    Add. NumberOfIsland_dfs

---
+++ src/NumberOfIsland_dfs.java
@@ -0,0 +1,47 @@
+public class NumberOfIsland_dfs {
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '1', '1'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        NumberOfIsland_dfs a = new NumberOfIsland_dfs();
+        System.out.println(a.numsIslands(grid));
+    }
+
+    int m, n;
+
+    int numsIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        m = grid.length;
+        n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    merge(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    void merge(char[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1')
+            return;
+
+        grid[i][j] = 'X';
+
+        merge(grid, i - 1, j);
+        merge(grid, i + 1, j);
+        merge(grid, i, j + 1);
+        merge(grid, i, j - 1);
+    }
+}
--- src/stackQueue/BaseballGame.java
+++ src/stackQueue/BaseballGame.java
@@ -1,3 +1,5 @@
+package stackQueue;
+
 import java.util.Stack;
 
 public class BaseballGame {
