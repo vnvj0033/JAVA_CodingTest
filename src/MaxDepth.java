456837b6575899ce0dcadb4f133acf7003f857bd
vnvj0033 <vnvj0033@gmail.com>
2021-04-17 19:46:10 +0900

    Add. MaxDepth Tree

---
+++ src/MaxDepth.java
@@ -0,0 +1,44 @@
+public class MaxDepth {
+
+    public static void main(String[] args) {
+        MaxDepth maxDepth = new MaxDepth();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.left = new TreeNode(6);
+
+        System.out.println(maxDepth.solve(root));
+    }
+
+    int max = 0;
+
+    public int solve(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+
+        max = Math.max(leftMax + rightMax, max);
+
+        return Math.max(leftMax, rightMax) + 1;
+
+    }
+
+    static private class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+    }
+}
