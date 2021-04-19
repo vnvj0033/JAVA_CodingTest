6f5c7e153b6d7b4b72d80336c666839c4b8a8ec6
vnvj0033 <vnvj0033@gmail.com>
2021-04-19 21:44:54 +0900

    Add. CoinChange

---
+++ src/CoinChange.java
@@ -0,0 +1,29 @@
+
+/**
+ * 코인을 이용해 최단 amount를 만들어라
+ * */
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 0;
+        System.out.println(coinChange(coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i)
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
