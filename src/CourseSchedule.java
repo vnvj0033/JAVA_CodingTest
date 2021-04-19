bcdae12415b005b203576705f04bcf1bd28e5629
vnvj0033 <vnvj0033@gmail.com>
2021-04-19 20:47:33 +0900

    Add. CourseSchedule

---
+++ src/CourseSchedule.java
@@ -0,0 +1,83 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 위상정렬 문제 : 사이클이 없는 그래프의 문제 풀이
+ * CourseSchedule : 코스를 수강하기 위한 전제조건 문제 풀이
+ * [0, 1]이 주어지면 0을 수강하기 위해 1을 완료 해야함
+ * num 배열이 위상 정렬인지 true, false 반환
+ * course는 수업 갯수
+ *
+ * input :
+ *   course = 4
+ *   num = { { 1, 0 },
+ *           { 2, 1 },
+ *           { 3, 2 } }
+ *
+ * output : true
+ *
+ *  0 -> 1 -> 2 -> 3
+ *
+ * */
+
+public class CourseSchedule {
+
+    public static void main(String[] args) {
+        int course = 4;
+        int[][] nums = { { 1, 0 },
+                { 2, 1 },
+                { 3, 2 } };
+
+        CourseSchedule a = new CourseSchedule();
+
+        System.out.println(a.solve(course, nums));
+    }
+
+    public boolean solve(int courseNumber, int[][] nums) {
+        if (courseNumber <= 0)
+            return false;
+
+        Queue<Integer> queue = new LinkedList<>();
+        int[] inDegree = new int[courseNumber];
+
+        // 1. inDegree : [1, 1, 1, 0]
+        int numsLength = nums.length;
+
+        for (int[] num : nums) inDegree[num[1]]++;
+
+        // 2.queue에 0인 값 찾아서 넣기
+        int completeLength = inDegree.length;
+
+        for (int i = 0; i < completeLength; i++)
+            if (inDegree[i] == 0)
+                queue.offer(i);
+
+        // 3.
+        while (!queue.isEmpty()) {
+            int zeroVal = queue.poll();
+
+            for (int i = 0; i < numsLength; i++) {
+
+                if (zeroVal == nums[i][0]) {
+                    System.out.println("=============");
+                    System.out.println("zeroVal "+zeroVal+" i "+i);
+                    inDegree[nums[i][1]]--;
+
+                    if (inDegree[nums[i][1]] == 0)
+                        queue.offer(nums[i][1]);
+                }
+            }
+        }
+
+        /*
+         *  4. 3번에서 completed에 값을 0으로 다 만들어야
+         *  완성completed[nums[i][1]]--
+         */
+
+        for (int i : inDegree)
+            if (i != 0)
+                return false;
+
+        return true;
+    }
+}
