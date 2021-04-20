dd47bd8349f256072ea4cbea51f4505ad1235bae
vnvj0033 <vnvj0033@gmail.com>
2021-04-20 22:16:02 +0900

    Fix. FindAnangramMapping

--- src/string/FindAnagramMapping.java
+++ src/string/FindAnagramMapping.java
@@ -2,6 +2,16 @@ package string;
 
 import java.util.HashMap;
 
+/**
+ * A의 문자를 키로 B에서의 위치를 찾는다.
+ * input :
+ *     A = {12, 28, 46, 32, 50}
+ *     B = {50, 12, 32, 46, 28}
+ *
+ * output : 1 4 3 2 0
+ * B의 원소를 키로 위치를 Map에 저장하고 A의 원소를 Map에서 찾는다.
+ * */
+
 public class FindAnagramMapping {
 
     public static void main(String[] args) {
