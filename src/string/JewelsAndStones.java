5ccfd58ef3f35bf19d03618eb353111e3cb57dc3
vnvj0033 <vnvj0033@gmail.com>
2021-04-20 22:55:53 +0900

    Fix. JewelsAndStones

--- src/string/JewelsAndStones.java
+++ src/string/JewelsAndStones.java
@@ -3,6 +3,17 @@ package string;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * S문자열에 J의 원소가 몇번 나오는지 검사
+ * input :
+ *     J = "aaA!"
+ *     S = "aAAbacsgbAbAf!@#ba"
+ *
+ * output : 8
+ *
+ * Set을 사용해 검사하면 간단하다.
+ * */
+
 public class JewelsAndStones {
 
     public static void main(String[] args) {
