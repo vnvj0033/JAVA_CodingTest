f020a0c22f3411704e23358a192fe6bc15fefa67
vnvj0033 <vnvj0033@gmail.com>
2021-04-20 23:01:20 +0900

    Fix. KClosest

--- src/string/KClosest.java
+++ src/string/KClosest.java
@@ -2,14 +2,26 @@ package string;
 
 import java.util.Arrays;
 
+/**
+ * 원점에서 가장먼 x, y 좌표 k개 구하기
+ * input :
+ *     points = {{1, 3}, {-2, 2}}
+ *     k = 1
+ *
+ * output : -2, 2
+ *
+ * 피타고라스 공식 사용하고. 결과 정렬해 순서 뽑기
+ * */
+
 public class KClosest {
 
     public static void main(String[] args) {
         int[][] points = {{1, 3}, {-2, 2}};
         int k = 1;
-        int[][] result = new KClosest().solve(points, k);
+        int[][] results = new KClosest().solve(points, k);
 
-        System.out.println(result[0][0] + ", " + result[0][1]);
+        for (int[] result : results)
+            System.out.println(result[0] + ", " + result[1]);
     }
 
     public int[][] solve(int[][] points, int k) {
