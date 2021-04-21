f719482d492c89928eb4a23d6dab66c944c0b284
vnvj0033 <vnvj0033@gmail.com>
2021-04-21 22:53:09 +0900

    Fix. MoveZeros

--- src/string/MoveZeros.java
+++ src/string/MoveZeros.java
@@ -1,17 +1,27 @@
 package string;
 
+/**
+ * 배열에서 0이 아닌수를 앞으로 이동하고 0은 뒤로 이동한다.
+ * input :
+ *     nums = {0, 3, 2, 0, 8, 5}
+ *
+ * output : {3, 2, 8, 5, 0, 0}
+ *
+ * */
+
 public class MoveZeros {
 
     public static void main(String args[]) {
         int[] nums = {0, 3, 2, 0, 8, 5};
 
         solve(nums);
+
         for (int num : nums)
             System.out.print(num + ",");
 
     }
 
-    private static int[] solve(int[] nums) {
+    private static void solve(int[] nums) {
         int currentIndex = 0;
 
         for (int i = 0; i < nums.length; i++)
@@ -20,7 +30,5 @@ public class MoveZeros {
 
         while (currentIndex < nums.length)
             nums[currentIndex++] = 0;
-
-        return nums;
     }
 }
