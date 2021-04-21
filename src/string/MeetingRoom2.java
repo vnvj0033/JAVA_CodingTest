a8012d662dad714b330ad2834731546d3a4920b5
vnvj0033 <vnvj0033@gmail.com>
2021-04-21 22:03:23 +0900

    Fix. MeetingRoom2

--- src/string/MeetingRoom2.java
+++ src/string/MeetingRoom2.java
@@ -5,6 +5,22 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * 미팅룸의 예약시간이 주어질때 최대 몇개의 룸이 필요한지
+ * input :
+ *     in1 = new Interval(4, 9);
+ *     in2 = new Interval(5, 15);
+ *     in3 = new Interval(0, 30);
+ *     in4 = new Interval(10, 16);
+ *
+ * output : 3
+ *
+ * Interval{start=5, end=15}
+ * Interval{start=0, end=30}
+ * Interval{start=10, end=16}
+ *
+ * PriorityQueue를 사용해 상위 정렬된 순서부터 비교하여 예약 시간을 추가한다.
+ * */
 
 public class MeetingRoom2 {
 
@@ -27,7 +43,6 @@ public class MeetingRoom2 {
         for (int i = 1; i < intervals.length; i++) {
             if (pq.peek().end <= intervals[i].start)
                 pq.poll();
-
             pq.add(intervals[i]);
         }
 
