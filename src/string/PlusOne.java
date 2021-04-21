a0874a619b4be0a98266413d447560aa6665e4cb
vnvj0033 <vnvj0033@gmail.com>
2021-04-21 22:57:54 +0900

    Fix. PlusOne

--- src/string/PlusOne.java
+++ src/string/PlusOne.java
@@ -1,31 +1,42 @@
 package string;
 
+/**
+ * 배열에 자리를 각각 10의 자리수로 파싱하여 더하기 1을 한 값을 배열로 반환
+ *
+ * input :
+ *     digits = {0, 9, 0}
+ *
+ * output : {0, 9, 1}
+ */
+
 public class PlusOne {
 
     public static void main(String[] args) {
         int[] digits = {0, 9, 0};
 
-        int[] result = new PlusOne().solve2(digits);
+        int[] result = new PlusOne().solve(digits);
 
         for (int i : result)
             System.out.print(i);
     }
 
     int[] solve(int[] digits) {
-        int index = digits.length - 1;
-        int carry = 1;
-        while (index >= 0 && carry == 1) {
-            digits[index] = (digits[index] + carry) % 10;
+        boolean isUp = false;
+        digits[digits.length - 1]++;
 
-            if (digits[index] == 0) {
-                carry = 1;
-            } else {
-                carry = 0;
+        for (int i = digits.length - 1; i > 0; i--) {
+            if (digits[i] > 9) {
+                digits[i] = 0;
+                isUp = true;
             }
-            index--;
+
+            if (isUp) {
+                digits[i - 1]++;
+                isUp = false;
+            }
         }
 
-        if (carry > 0) {
+        if (isUp) {
             digits = new int[digits.length + 1];
             digits[0] = 1;
         }
@@ -34,23 +45,19 @@ public class PlusOne {
     }
 
     int[] solve2(int[] digits) {
-        boolean isUp = false;
-        digits[digits.length-1]++;
+        int index = digits.length - 1;
+        int carry = 1;
+        while (index >= 0 && carry == 1) {
+            digits[index] = (digits[index] + carry) % 10;
 
-        for (int i = digits.length -1; i > 0; i--) {
-            if (digits[i] > 9) {
-                digits[i] = 0;
-                isUp = true;
-            }
+            if (digits[index] == 0) carry = 1;
+            else carry = 0;
 
-            if (isUp) {
-                digits[i-1]++;
-                isUp = false;
-            }
+            index--;
         }
 
-        if (isUp){
-            digits = new int[digits.length+1];
+        if (carry > 0) {
+            digits = new int[digits.length + 1];
             digits[0] = 1;
         }
 
