9b0f60382e202a8091cac6d393b788e83232af91
vnvj0033 <vnvj0033@gmail.com>
2021-04-21 22:46:19 +0900

    Fix. MissingRanges

--- src/string/MissingRanges.java
+++ src/string/MissingRanges.java
@@ -2,6 +2,16 @@ package string;
 
 import java.util.*;
 
+/**
+ * lower과 upper 범위 내에 제외된 숫자를 찾는다.
+ * input :
+ *     nums = {0, 2, 3, 5, 50, 75, 99}
+ *     lower = 0
+ *     upper = 99
+ *
+ * output : [1, 4, 6->49, 51->74, 76->98]
+ * */
+
 public class MissingRanges {
     public static void main(String[] args) {
 
