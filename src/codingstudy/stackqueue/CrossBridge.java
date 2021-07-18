b6ab397997e2abea82148eea36dfee285dadf9a3
vnvj0033 <vnvj0033@gmail.com>
2021-07-18 10:56:24 +0900

    다리 건너기 리팩토링

--- src/codingstudy/stackqueue/CrossBridge.java
+++ src/codingstudy/stackqueue/CrossBridge.java
@@ -3,16 +3,22 @@ package codingstudy.stackqueue;
 import java.util.*;
 
 public class CrossBridge {
+
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = bridge_length;
+        int answer = crossBridge(bridge_length, weight, truck_weights);
+
+        return answer;
+    }
+
+    private int crossBridge(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> bridgeQueue = new LinkedList<>();
 
         int bridgeWeight = 0;
-
-        Queue<Integer> bridgeQueue = new LinkedList<>();
+        int result = bridge_length;
 
         for (int truck : truck_weights){
             while (true) {
-                answer++;
+                result++;
 
                 if (bridgeQueue.size() == bridge_length)
                     bridgeWeight -= bridgeQueue.poll();
@@ -27,7 +33,6 @@ public class CrossBridge {
                 break;
             }
         }
-
-        return answer;
+        return result;
     }
 }
