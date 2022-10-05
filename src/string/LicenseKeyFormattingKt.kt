a5af0f4063b5ebc2854f64518cb9c1d36f84d6f7
SangYeob <vnvj0033@gmail.com>
2022-10-05 21:25:36 +0900

    solve LicenseKeyFormattingKt.kt

---
+++ src/string/LicenseKeyFormattingKt.kt
@@ -0,0 +1,34 @@
+package string
+
+class LicenseKeyFormattingKt {
+    fun solve(s: String, k: Int): String {
+
+        val str = s.replace("-", "").uppercase()
+
+        var sb = StringBuffer()
+
+        str.reversed().forEachIndexed { index, c ->
+            if (index % k == 0) {
+                sb.append("-")
+            }
+
+            sb.append(c)
+
+        }
+
+        if (sb[0] == '-') {
+            sb.deleteCharAt(0)
+        }
+
+        return sb.toString().reversed()
+    }
+}
+
+fun main() {
+
+    val J = "8F3Z-2e-9-w"
+    val S = 4
+
+    println(LicenseKeyFormattingKt().solve(J, S))
+
+}
