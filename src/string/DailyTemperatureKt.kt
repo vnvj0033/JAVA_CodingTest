9c56e32ba7d368f0a35772483da8440d2fdda5e1
yoosangyeob <syyoo@joara.com>
2022-10-08 18:57:34 +0900

    solve DailyTemperature

---
+++ src/string/DailyTemperatureKt.kt
@@ -0,0 +1,29 @@
+package string
+
+class DailyTemperatureKt {
+    fun solve(nums: Array<Int>): Array<Int> {
+        val result = Array(nums.size) {0}
+
+        nums.forEachIndexed { i1, n1 ->
+            for (i2 in i1+1 until nums.size) {
+                val n2 = nums[i2]
+                if (n1 > n2) {
+                    continue
+                } else {
+                    result[i1] = i2 - i1
+                    break
+                }
+            }
+        }
+
+        return result
+    }
+}
+
+fun main() {
+
+    val nums = arrayOf(73, 74, 75, 71, 69, 72, 76, 73)
+    val results = DailyTemperatureKt().solve(nums)
+
+    for (result in results) print("$result ")
+}
